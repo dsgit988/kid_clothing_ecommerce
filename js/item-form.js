1939de5735e827603278c4524d04c17bb0644cef
Tom <pjoh1092@gmail.com>
2022-12-15 15:56:44 -0600

    revise item-form and connect form the postmapping etc

--- js/item-form.js
+++ js/item-form.js
@@ -2,6 +2,8 @@ import { ItemsController } from "./itemsController.js";
 const itemsController = new ItemsController(0);
 
 const newProductForm = document.getElementById("newProductForm");
+const updateProductForm = document.getElementById("updateProductForm");
+const deleteProductForm = document.getElementById("deleteProductForm");
 
 newProductForm.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -51,6 +53,84 @@ newProductForm.addEventListener("submit", (event) => {
     localStorage.setItem("products", JSON.stringify(products));
 
 });
+
+updateProductForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const idElementToUpdate = document.getElementById("idToUpdate");
+    const nameElementToUpdate = document.getElementById("nameToUpdate");
+    const descriptionElementToUpdate = document.getElementById("descriptionToUpdate");
+    const imageElementToUpdate = document.getElementById("imageToUpdate");
+    const priceElementToUpdate = document.getElementById("priceToUpdate");
+    const dateElementToUpdate = document.getElementById("dateToUpdate");
+
+    const id = idElementToUpdate.value.trim();
+    const name = nameElementToUpdate.value.trim();
+    const description = descriptionElementToUpdate.value.trim();
+    const image = imageElementToUpdate.value.trim();
+    const price = priceElementToUpdate.value.trim();
+    const date = dateElementToUpdate.value.trim();
+
+    //form validation
+    if (id === "" | name === "" || description === "" || image === "" || date === ""){
+        alert("Input can not be all whitespace, please try again");
+        return;
+    }else if (isNaN(price)){
+        alert("Price must be a number, please try again.");
+        return;
+    }
+
+    
+
+    itemsController.update(id, name, description, image, price, date);
+
+    idElementToUpdate.value = "";
+    nameElementToUpdate.value = "";
+    descriptionElementToUpdate.value = "";
+    imageElementToUpdate.value = "";
+    priceElementToUpdate.value = "";
+    dateElementToUpdate.value = "";
+
+
+    // const addProductToLocalStorage = 
+    //     {
+    //         'name': name,
+    //         'img': image,
+    //         'description': description,
+    //         'price': '$' + price,
+    //         'createdAt': date
+    //     }
+    
+
+    // let products = JSON.parse(localStorage.getItem('products')) || [];
+    // products.push(addProductToLocalStorage);
+    // localStorage.setItem("products", JSON.stringify(products));
+
+});
+
+deleteProductForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const idToDelete = document.getElementById("idToDelete");
+
+
+    const id = idToDelete.value.trim();
+ 
+
+    //form validation
+    if (id === ""){
+        alert("ID can not be all whitespace, please try again");
+        return;
+    }
+
+    
+
+    itemsController.delete(id);
+
+    idToDelete.value = "";
+ 
+
+
+});
+
 
 
 
--- js/itemsController.js
+++ js/itemsController.js
@@ -15,10 +15,12 @@ class ItemsController{
 
         }
         this.items.push(item);
+        this.save(name, description, img, price);
         
     }
 
     loadProductsFromLocalStorage() {
+
         const storeProducts = localStorage.getItem("products")
         if (storeProducts) {
             const products = JSON.parse(storeProducts)
@@ -27,7 +29,77 @@ class ItemsController{
                 this.items.push(product);
             }
         }
+
+
+/*         try{
+            const response = await fetch("http://localhost:8080/api/items");
+            const products = await response.json();
+            
+            for (let i = 0, size = products.length; i < size; i++) {
+                const product = products[i];
+                this.items.push(product);
+            }
+            
+        }catch(e){
+            console.log("oh, no " + e.message);
+        } */
+
     }
+
+    save(name, description, imageUrl, price){
+        const data = { "name": name,  "description": description, "imageUrl": imageUrl, "price": price};
+
+        fetch('http://localhost:8080/api/items', {
+        method: 'POST', // or 'PUT'
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(data => {
+        console.log('Success:', data);
+        })
+        .catch((error) => {
+        console.error('Error:', error);
+        });
+    }
+
+    update(id, name, description, imageUrl, price){
+        const data = { "name": name,  "description": description, "imageUrl": imageUrl, "price": price};
+
+        fetch('http://localhost:8080/api/items/' + id, {
+        method: 'PUT', // or 'PUT'
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(data => {
+        console.log('Success:', data);
+        })
+        .catch((error) => {
+        console.error('Error:', error);
+        });
+    }
+
+    delete(id){
+        
+
+        fetch('http://localhost:8080/api/items/' + id, {
+        method: 'DELETE'
+        })
+        .then(response => response.json())
+        .then(data => {
+        console.log('Success:', data);
+        })
+        .catch((error) => {
+        console.error('Error:', error);
+        });
+    }
+
+
 }
 
 export {ItemsController};
